228d62f6ebd3120f50b850866eb5aac2c96e3f19
o4tapabrato <www.tapabratomaity@gmail.com>
2026-09-16 12:42:17 +0530

    adjusted bulk

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["@prisma/client", "pg"],
-  experimental: {
-    turbo: {
-      resolveAlias: {
-        ".prisma/client/default": "./node_modules/.prisma/client/default.js",
-      },
-    },
-  },
 };
 
 export default nextConfig;
