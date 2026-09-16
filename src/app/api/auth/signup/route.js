67f10cfbe3a54e0abc5d9f3951c10417b950e3b3
o4tapabrato <www.tapabratomaity@gmail.com>
2026-09-16 12:24:53 +0530

    Added survey analytics

--- src/app/api/auth/signup/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/app/lib/prisma";
-
-export async function POST(request) {
-    const body = await request.json();
-    const { username, password } = body;
-}
